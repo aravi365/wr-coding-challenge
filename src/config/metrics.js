6cfbe8a52102782ffb0f78bf3f5a3f87e694c7a6
Aravind <aravind.nair2010@gmail.com>
2021-07-18 16:32:48 +0530

    add helper method for screen dimensions

---
+++ src/config/metrics.js
@@ -0,0 +1,12 @@
+/*
+ * platform/application wide metrics for proper styling
+ */
+import {Dimensions, Platform} from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+const metrics = {
+  screenWidth: width < height ? width : height,
+  screenHeight: width < height ? height : width,
+};
+
+export default metrics;
