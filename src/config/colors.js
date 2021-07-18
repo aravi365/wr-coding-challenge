e779cec688c489395c6b4499325a75a21caa22b4
Aravind <aravind.nair2010@gmail.com>
2021-07-18 16:32:56 +0530

    add color config file

---
+++ src/config/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  white: '#ffffff',
+  grey: 'rgb(153,153,153)',
+  warmGrey: 'rgb(153,153,153)',
+  brownishGrey: 'rgb(108,108,108)',
+  charcoalGrey: 'rgba(60, 60, 67, 1)',
+};
+
+export default colors;
